64ee2e1d8801308cc2f61f65fec3f0267ca2a620
maktuta <121550063+maktuta@users.noreply.github.com>
2025-12-18 21:28:30 +0200

    Delete src directory

--- src/main/java/org/example/lesson_2/persistence/projection/TopProductProjection.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_2.persistence.projection;
-
-public interface TopProductProjection {
-    Long getProductId();
-    String getProductName();
-    Long getTotalQuantity();
-}
--- src/main/java/org/example/lesson_2/persistence/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_2.persistence.repository;
-
-import org.example.lesson_2.persistence.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-}
